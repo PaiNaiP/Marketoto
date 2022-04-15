e8b0bb8960d70325f9549bd74cabec8f6d77e4a8
PaiNaip <katesnow2002@gmail.com>
2022-04-15 13:56:14 +0300

    hi

---
+++ js/pass.js
@@ -0,0 +1,12 @@
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDRlNhNaO0o1xKYhPIjAKO2Ko5Drz7vxLM",
+    authDomain: "marketoto-d7c0e.firebaseapp.com",
+    projectId: "marketoto-d7c0e",
+    storageBucket: "marketoto-d7c0e.appspot.com",
+    messagingSenderId: "674088598405",
+    appId: "1:674088598405:web:7a02f66abeffb8512be2ec",
+    measurementId: "G-5WJYPXVF1Z"
+  };
